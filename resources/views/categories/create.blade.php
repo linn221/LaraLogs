672dc5594e68475436e6131c761168a5132b75c8
linn <linncoffeee@gmail.com>
2023-06-27 22:48:49 +0630

    finish view layout for categories

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    Create Category
+    <div class=" container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Create New Category</h3>
+                <hr>
+                <form action="{{ route('dummy') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label class=" form-label" for="">Category Title</label>
+                        <input type="text" class=" form-control @error('title') is-invalid @enderror"
+                            value="{{ old('title') }}" name="title">
+                        @error('title')
+                            <div class=" invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <button class=" btn btn-primary">Save Category</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
-
 @section('content')
-    Categories, categories, everywhere...
+    <div class=" container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Category List</h3>
+                <hr>
+
+                <div class=" mb-3">
+                    <a href="{{ route('categories.create') }}" class="btn btn-outline-dark">Create</a>
+                </div>
+
+                <table class=" table">
+                    <tr>
+                        <td>
+                            $category_name ($logs_count) (link to see logs under the category)
+                        </td>
+                    </tr>
+                </table>
+                <div class="">
+                    {{-- {{ $categories->onEachSide(1)->links() }} --}}
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
