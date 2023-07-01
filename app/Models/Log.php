7b6ca80ebace5f2b4fef5e66a642de813a54ffaf
linn <linncoffeee@gmail.com>
2023-07-01 16:30:56 +0630

    defined relationship between log and category

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Log extends Model
 {
     use HasFactory;
+
+    public function Category() {
+        return $this->belongsTo(Category::class);
+    }
 }
