ca2eabf5afa081faa8dbfc25d7fb238cc695c922
linn <linncoffeee@gmail.com>
2023-07-14 17:26:04 +0630

    finish email controller brief code and  routes

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEmailRequest;
+use App\Http\Requests\UpdateEmailRequest;
+use App\Models\Email;
+use Carbon\Carbon;
+
+class EmailController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    private function mail($action, $link, $email) {
+        logger("$action email for $email->address with $link link");
+    }
+
+    public function subscribe(StoreEmailRequest $request)
+    {
+        $email = new Email();
+        $email->address = $request->input('address');
+        $email->token = fake()->password(18);
+        $email->save();
+        // send email on observer
+        $this->mail('subscribe', 'unsubscribe', $email);
+        return redirect()->back()->with(['status' => 'YOu now have active scription to new posts. You can unsubscribe from the link in the email but be cautious becuase you can only subscribe again by the re-subscribe link for non-verified accounts']);
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function verify(UpdateEmailRequest $request, Email $email)
+    {
+        // yellow, will be better to do it in Requests
+        if ($email->token == $request->input('token')) {
+            $email->token = fake()->password(16);
+            $email->verfied_at = now();
+            $this->mail('verification success', 'delete email', $email);
+            return redirect()->back();
+        }
+        return redirect()->back()->with(['status' => 'Invalid verification request']);
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function cancel(UpdateEmailRequest $request, Email $email)
+    {
+        if ($email->token == $request->input('token')) {
+            $email->subscribed_at = null;
+            $email->token = fake()->password(16);
+            $email->save();
+            $this->mail('canceled email', 're-subscribe', $email);
+            return redirect()->back();
+        }
+        return redirect()->back()->with(['status' => 'Invalid verification request']);
+        //
+    }
+
+    public function resubscribe(UpdateEmailRequest $request, Email $email)
+    {
+        if ($email->token == $request->input('token')) {
+            $email->subscribed_at = now();
+            $email->verified = now();
+            $email->token = fake()->password(16);
+            $email->save();
+            $this->mail('verification success', 'delete email', $email);
+            return redirect()->back()->with(['status' => 'You have resubscribed and verified your email address. you may delete the subscription by link in new email, thanks']);
+        }
+        return redirect()->back()->with(['status' => 'Invalid verification request']);
+    }
+}
---
+++ app/Http/Requests/StoreEmailRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEmailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'address' => 'required|email'
+            //
+        ];
+    }
+
+    public function messages()
+    {
+            $status  = 'You already have an active subscription. No need to subscribe twice. in case you have canceled subscription, you can subscribe back by clicking the re-subscribe link in our email. thanks';
+        
+    }
+}
