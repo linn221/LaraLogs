cb8f05263c93c9a55ae5633e336d2e8906bf6699
linn <linncoffeee@gmail.com>
2023-08-23 18:43:34 +0630

    add admin user on seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => env("ADMIN_NAME"),
+            'email' => env('ADMIN_EMAIL'),
+            'password' => env('ADMIN_PASSWORD')
         ]);
     }
 }
